49b428b448915229f39beffe5529bc66d935ba16
Brian Ford <brian@flipsidecrypto.com>
2020-07-08 16:02:49 -0400

    Update name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='chainwalkers_utils',
+    name='ChainwalkersUtils',
     version='0.0.1',
     description='Collection of utilities to be used across chainwalkers repos',
     url='git@github.com:FlipsideCrypto/chainwalkers-utils.git',
