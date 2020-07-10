fcdff1fcad3a9e663e18c82203be82cadec4a532
Brian Ford <brian@flipsidecrypto.com>
2020-07-10 10:22:38 -0400

    Untrack build files

--- build/lib/eth_utils/decimals.py
+++
@@ -1,7 +0,0 @@
-def hex_to_decimal(hex_value):
-    if hex_value == '0x':
-        return 0
-    return int(hex_value, 16)
-
-def decimal_to_hex(decimal):
-    return hex(decimal)
--- build/lib/tendermint_utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from tendermint.rpc import TendermintRPC
